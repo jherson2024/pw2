ec0a5fb9ef3bb9a01d5e15bfb013d8c03c30d3a8
Jherson <jinca@unsa.edu.pe>
2024-05-23 14:24:54 -0500

    agregando un index.html

--- practicaSQLlite/server.js
+++ practicaSQLlite/server.js
@@ -3,7 +3,15 @@ const sqlite3=require("sqlite3").verbose();
 
 const PORT=process.env.PORT||3000;
 const app=express();
-const db=new sqlite3.Database("imbd.db");
+const db=new sqlite3.Database("imdb.db");
+
+app.use(express.static(__dirname));
+
+app.get("/movies",(req,res)=>{
+    db.all("select*from movies",(err,rows)=>{
+        res.json(rows);
+    });
+});
 
 app.listen(PORT,()=>{
     console.log(`server is runnig on http://localhost:${PORT}`);
