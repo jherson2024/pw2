6b8dc24106cc00f63d8e02c0e5636627b2ba21ae
Jherson <jinca@unsa.edu.pe>
2024-05-26 15:48:34 -0500

    añadir formulario CursoForm

--- LabDjango/Ejercicio_propuesto/sistema_alumnos/registro_notas/forms.py
+++ LabDjango/Ejercicio_propuesto/sistema_alumnos/registro_notas/forms.py
@@ -3,5 +3,10 @@ from .models import Alumno, Curso, Nota
 
 class AlumnoForm(forms.ModelForm):
     class Meta:
-        model = Alumno
-        fields = ['nombre']
+        model=Alumno
+        fields=['nombre']
+
+class CursoForm(forms.ModelForm):
+    class Meta:
+        model=Curso
+        fields=["nombre"]
