950c345ca91103712ea21090bbfeb1416ec53361
Jherson <jinca@unsa.edu.pe>
2024-05-26 16:06:27 -0500

    crear formularios vacios para alumno, curso y nota

--- LabDjango/Ejercicio_propuesto/sistema_alumnos/registro_notas/views.py
+++ LabDjango/Ejercicio_propuesto/sistema_alumnos/registro_notas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 from .models import Alumno, Curso, Nota
+from .forms import AlumnoForm, CursoForm, NotaForm
+
 # Create your views here.
+def pagina(request):
+    alumno_form=AlumnoForm()
+    curso_form=CursoForm()
+    nota_form=NotaForm()
