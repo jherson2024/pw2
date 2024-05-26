5b15d3dc67af896fa9fcdba0b1862e084430eb0b
Jherson <jinca@unsa.edu.pe>
2024-05-26 16:45:49 -0500

    en url asignar la pagina_principal al directorio fuente

--- LabDjango/Ejercicio_propuesto/sistema_alumnos/sistema_alumnos/urls.py
+++ LabDjango/Ejercicio_propuesto/sistema_alumnos/sistema_alumnos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from registro_notas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.pagina_principal, name='pagina_principal'),
 ]
