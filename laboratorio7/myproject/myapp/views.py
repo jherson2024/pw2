7398373947047950f3f24ebb585e00f9dd77c36a
Jherson <jinca@unsa.edu.pe>
2024-06-01 06:03:23 -0500

    si se usa comillas simples se puede obtener un entero anteponiendo (int)

--- laboratorio7/myproject/myapp/views.py
+++ laboratorio7/myproject/myapp/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request,'index.html',{"name":"Navin"})  
 
 def add(request):   
-    val1=request.GET["num1"]
-    val2=request.GET["num2"]
+    val1=int(request.GET['num1'])
+    val2=int(request.GET['num2'])
     res=val1+val2
     return render(request,'result.html',{"result":res})   
