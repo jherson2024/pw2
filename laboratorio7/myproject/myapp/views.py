3a8dfafbe5bd86cdc1dc127c9b6d3b9c208d6d64
Jherson <jinca@unsa.edu.pe>
2024-06-01 05:57:36 -0500

    prueba sumando los datos del form y enviar el resultado a un html

--- laboratorio7/myproject/myapp/views.py
+++ laboratorio7/myproject/myapp/views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'index.html',{"name":"Navin"})  
 
-def add(request):
-    return render(request,'result.html')   
+def add(request):   
+    val1=request.GET["num1"]
+    val2=request.GET["num2"]
+    res=val1+val2
+    return render(request,'result.html',{"result":res})   
