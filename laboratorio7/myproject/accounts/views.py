dbe42ec9e8c14b0f82a5bb811bef144df8bbe261
Jherson <jinca@unsa.edu.pe>
2024-06-04 12:31:59 -0500

    añadiendo condicional si los password no son iguales

--- laboratorio7/myproject/accounts/views.py
+++ laboratorio7/myproject/accounts/views.py
@@ -10,9 +10,12 @@ def register(request):
         password1=request.POST["password1"]
         password2=request.POST["password2"]
         email=request.POST["email"]
-        user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
-        user.save();
-        print("user created")
-        return redirect("/")
+        if password1==password2:
+         user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+         user.save();
+         print("user created")
+         return redirect("/")
+        else: 
+           print("password not matchine..")
     else:
         return render(request,"register.html")
