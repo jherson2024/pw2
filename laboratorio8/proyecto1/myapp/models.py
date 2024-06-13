969ac745bad6e71c22d687eec2b1ea446aa5e191
Jherson <jinca@unsa.edu.pe>
2024-06-13 11:29:44 -0500

    agregar los modelos Movie y Character con una relacion de muchos a muchos

--- laboratorio8/proyecto1/myapp/models.py
+++ laboratorio8/proyecto1/myapp/models.py
@@ -25,3 +25,14 @@ class Framework(models.Model):
     language=models.ForeignKey(Language, on_delete=models.CASCADE)
     def __str__(self):
         return self.name 
+    
+class Movie(models.Model):
+    name=models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
+    
+class Character(models.Model):
+    name=models.CharField(max_length=10)
+    movies=models.ManyToManyField(Movie)
+    def __str__(self):
+        return self.name
