88a6ea2afa8edfb6b9831c7a2ab2aecae8768711
Jherson <jinca@unsa.edu.pe>
2024-06-14 09:50:56 -0500

    agregar la vista para renderizar la pagina

--- laboratorio8/proyecto1/myapp/urls.py
+++ laboratorio8/proyecto1/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.pagina_principal, name='pagina_principal'),
+    path('pdf/',views.render_pdf_view, name='render_pdf'),
 ]
--- laboratorio8/proyecto1/myapp/views.py
+++ laboratorio8/proyecto1/myapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from xhtml2pdf import pisa
+from django.template.loader import get_template
 
 def pagina_principal(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
+def render_pdf_view(request):
+    context = {
+        "content": "",
+    }
+    template_path = 'index.html'
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = "attachment; filename='archivo.pdf'"
+    template = get_template(template_path)
+    html = template.render(context)
+    pisa_status = pisa.CreatePDF(html, dest=response, encoding="utf-8")
+    if pisa_status.err:
+        return HttpResponse("Hubo errores al generar el PDF: %s" % html)
+    return response
