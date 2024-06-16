52ffb473b8a85f74acdf585e455cfa7ac6a2bcc5
Jherson <jinca@unsa.edu.pe>
2024-06-15 21:11:27 -0500

    definiendo la vista para buscar una persona

--- tareaDjango3/proyecto1/myapp/urls.py
+++ tareaDjango3/proyecto1/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.myHomeView, name='myHomeView'),
     path('persona/',views.personaTestView,name='testViewPersona'),
-    path('agregar/',views.personaCreateView,name='createPersona'), 
+    path('agregar/',views.personaCreateView,name='createPersona'),
+    path('search/',views.searhForHelp,name='buscar'), 
 ]
