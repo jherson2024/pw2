cc4768511dbd6c146c4a0ddfc682b65a8430ebbb
Jherson <jinca@unsa.edu.pe>
2024-06-15 19:35:46 -0500

    en la vista crear un diccionario y enviar a la función render

--- tareaDjango3/proyecto1/myapp/views.py
+++ tareaDjango3/proyecto1/myapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def myHomeView(request,*args,**kwargs):
-    return render(request,"home.html",{})
+    myContext={
+        "myText":"Esto es sobre nosotros",
+        "myNumber":123,
+    }
+    return render(request,"home.html",myContext)
