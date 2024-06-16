98a3c705af047b359038ee8eb229aa17a06a4fb1
Jherson <jinca@unsa.edu.pe>
2024-06-15 20:58:40 -0500

    definiendo la vista para un formulario sencillo de Persona

--- tareaDjango3/proyecto1/myapp/views.py
+++ tareaDjango3/proyecto1/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 # Create your views here.
 def myHomeView(request,*args,**kwargs):
     myContext={
@@ -15,3 +16,14 @@ def personaTestView(request):
         "objeto":obj,
     }
     return render(request,"personas/descripcion.html",context)
+
+def personaCreateView(request):
+    form=PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=PersonaForm()
+
+    context={
+        "form":form
+    }
+    return render(request,"personas/personasCreate.html",context)
