33edb8d7bde44780cb603234ea3fe616e4df3900
Jherson <jinca@unsa.edu.pe>
2024-06-15 20:21:40 -0500

    agregando el modelo persona y realizar las moigraciones

--- tareaDjango3/proyecto1/myapp/models.py
+++ tareaDjango3/proyecto1/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombres=models.CharField(max_length=100)
+    apellidos=models.CharField(max_length=100)
+    edad=models.IntegerField(blank=True)
+    donador=models.BooleanField()
