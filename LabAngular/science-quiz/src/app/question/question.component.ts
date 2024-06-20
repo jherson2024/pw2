93c86bf31b2927c87429cc4cf571d23a8c1848be
Jherson <jinca@unsa.edu.pe>
2024-06-20 14:21:19 -0500

    añadiendo la lógica para cargar y mostrar las preguntas

--- LabAngular/science-quiz/src/app/question/question.component.ts
+++ LabAngular/science-quiz/src/app/question/question.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuestionService } from '../question.service';
 
 @Component({
   selector: 'app-question',
-  standalone: true,
-  imports: [],
   templateUrl: './question.component.html',
-  styleUrl: './question.component.css'
+  styleUrls: ['./question.component.css']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
+  question: any;
+  currentIndex: number=0;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private questionService: QuestionService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.currentIndex = +params['id'];
+      this.question = this.questionService.getQuestions()[this.currentIndex];
+    });
+  }
+
+  nextQuestion(): void {
+    if (this.currentIndex < this.questionService.getQuestions().length - 1) {
+      this.router.navigate(['/question', this.currentIndex + 1]);
+    } else {
+      this.router.navigate(['/results']);
+    }
+  }
 }
