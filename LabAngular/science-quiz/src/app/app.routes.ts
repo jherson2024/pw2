42809f10f08a012c3290b981ed7b8c6f5da7ef14
Jherson <jinca@unsa.edu.pe>
2024-06-20 14:23:18 -0500

    añadiendo las rutas para los componentes

--- LabAngular/science-quiz/src/app/app.routes.ts
+++ LabAngular/science-quiz/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultsComponent } from './results/results.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question/:id', component: QuestionComponent },
+  { path: 'results', component: ResultsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
