fc3912c5367c9ba3cc5792d06d556bb7ae87002a
Jherson <jinca@unsa.edu.pe>
2024-06-23 21:44:18 -0500

    crear un modelo PersonaDetailView

--- django5/myproject/myapp/models.py
+++ django5/myproject/myapp/models.py
@@ -12,3 +12,7 @@ class Persona(models.Model):
 class PersonaListView(ListView):
     model=Persona
     queryset=Persona.objects.filter(edad__lte="40") 
+
+class PersonaDetailView(DetailView):
+    model=Persona
+    # queryset=Persona.objects.filter(edad__lte="40") 
