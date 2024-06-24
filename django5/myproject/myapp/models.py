329b895eb736fc61262cecb576c1807c884bdc0a
Jherson <jinca@unsa.edu.pe>
2024-06-23 21:35:29 -0500

    crear el modelo Persona

--- django5/myproject/myapp/models.py
+++ django5/myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombres=models.CharField(max_length=100)
+    apellidos=models.CharField(max_length=100)
+    edad=models.IntegerField()
+    donador=models.BooleanField()
+    def get_abosolute_url(self):
+        return reverse("personas:persona-detail",kwards={"pk":self.id})
