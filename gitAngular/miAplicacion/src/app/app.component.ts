709870962ed9aa58d95ce715ff04cd5694699f3f
Jherson <jinca@unsa.edu.pe>
2024-07-12 09:52:16 -0500

    mostrar datos del servicio data en app.components.ts

--- gitAngular/miAplicacion/src/app/app.component.ts
+++ gitAngular/miAplicacion/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule],
+  providers:[DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'miAplicacion';
+  constructor(private dataService:DataService){
+    this.dataService.getData().subscribe(data=>{
+      console.log(data);
+    });
+  }
 }
