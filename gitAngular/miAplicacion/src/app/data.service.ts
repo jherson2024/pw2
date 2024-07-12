e50ed72767a41f4a3d8e86ce7476dcca1c6b14a3
Jherson <jinca@unsa.edu.pe>
2024-07-12 09:51:22 -0500

    método en dataService para obtener data de https://jsonplaceholder.typicode.com/posts

--- gitAngular/miAplicacion/src/app/data.service.ts
+++ gitAngular/miAplicacion/src/app/data.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) {
+    console.log("Service working...");
+   }
+  getData(): Observable<any> {
+    return this.httpClient.get<any>("https://jsonplaceholder.typicode.com/posts");
+  }
 }
