3a2460b827cad3603d935a9f90f085e27753f0d9
Jherson <jinca@unsa.edu.pe>
2024-07-12 10:15:52 -0500

    agregando la interface Post

--- gitAngular/miAplicacion/src/app/data.service.ts
+++ gitAngular/miAplicacion/src/app/data.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
   constructor(private httpClient:HttpClient) {
     console.log("Service working...");
    }
-  getData(): Observable<any> {
-    return this.httpClient.get<any>("https://jsonplaceholder.typicode.com/posts");
+  getData(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
   }
 }
