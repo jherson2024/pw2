1c9b8b82091bf6c33ddfc7739d62afab595d1c82
Jherson <jinca@unsa.edu.pe>
2024-07-12 10:13:04 -0500

    añadir un arreglo posts para almacenar los datos de dataService e importar el servicio data y commonModule para usar *ng

--- gitAngular/miAplicacion/src/app/app.component.ts
+++ gitAngular/miAplicacion/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataService } from './data.service';
+import { DataService, Post } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule],
+  imports: [RouterOutlet,HttpClientModule,CommonModule],
   providers:[DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'miAplicacion';
+  posts: Post[]=[];
   constructor(private dataService:DataService){
     this.dataService.getData().subscribe(data=>{
-      console.log(data);
+      // console.log(data);
+      this.posts=data;
     });
   }
 }
