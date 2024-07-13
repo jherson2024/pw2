64f616b6b1fa5e12e739e2946e9199e8afd2ede5
Jherson <jinca@unsa.edu.pe>
2024-07-13 12:03:55 -0500

    importando routerLink y httpClientModule

--- gitDjangular/crud/src/app/app.component.ts
+++ gitDjangular/crud/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,HttpClientModule,],
   providers:[ApiService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
